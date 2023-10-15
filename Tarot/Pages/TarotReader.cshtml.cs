603a1a92b8d2bb3b150bde37138adf23177a6ed3
Quanvo313 <vhquan313@gmail.com>
2023-10-15 21:41:07 +0700

    add login, register, reader frontend

---
+++ Tarot/Pages/TarotReader.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarot.Pages
+{
+    public class TarotReader : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TarotReader(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
