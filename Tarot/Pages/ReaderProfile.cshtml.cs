dedba79f7848c55e2378047ade3d2488c537122d
Quanvo313 <vhquan313@gmail.com>
2023-10-17 16:34:33 +0700

    reader profile

---
+++ Tarot/Pages/ReaderProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarot.Pages
+{
+    public class ReaderProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
